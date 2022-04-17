d89bfdb7db7ef4499c6a9b095ae58aa101e90182
jmoseka <jamylam3@gmail.com>
2022-04-17 12:39:29 +0300

    add speaker image links

--- index.js
+++ index.js
@@ -26,42 +26,42 @@ document.onclick = (e) => {
 
 const speakers = {
   column1: {
-    image: 'images/speakers/img-1.jpg',
+    image: 'https://i.postimg.cc/66cdYZfN/linkedin-sales-solutions-p-At-A8xe-i-VM-unsplash.jpg',
     names: 'Yochai Benki',
     profession: 'Berkman Professor of Enterprenuerial Legal Studies at Harvad School of Law',
     function: 'Berkler studies commons-based peer production and published his seminal book: The Wealth of Networks',
   },
 
   column2: {
-    image: 'images/speakers/img-1.jpg',
+    image: 'https://i.postimg.cc/mDwFpxMS/thanh-duc-phan-i5-R8hb-ZFDdc-unsplash.jpg',
     names: 'Sheila Kizuga',
     profession: '',
     function: 'Sheila Kizuga helped bring the internet to Africa and is an outspoken advocate for the open web and digital commons. In 2012, she was inducted into the inaugral class of the internet Society (ISOC) hall of fame',
   },
 
   column3: {
-    image: 'images/speakers/img-2.jpg',
+    image: 'https://i.postimg.cc/3Jb2HHqS/joseph-gonzalez-i-Fg-Rcq-Hznqg-unsplash-1.jpg',
     names: 'Lee Pak',
     profession: 'Director of Art Center and a board of member of Maua Academy',
     function: 'As the main avenue for new media art production in Kenya, Maua promotes criss-discplinary collaboration and understanding among science technology and the arts',
   },
 
   column4: {
-    image: 'images/speakers/img-4.jpg',
+    image: 'https://i.postimg.cc/ydBZT6Jd/prince-akachi-J1-OScm-u-HUQ-unsplash-1.jpg',
     names: 'Amara Akachi',
     profession: 'Presidents on Young Ineventors Stars',
     function: 'Youth intergration, political democracy and paticipation of youth through online as her major condem. Amaras report outlining potential changes to AU copyright law was approved by the Parliament July',
   },
 
   column5: {
-    image: 'images/speakers/img-4.jpg',
+    image: 'https://i.postimg.cc/8PCL3CZf/michael-dam-m-EZ3-Po-FGs-k-unsplash.jpg',
     names: 'Queen Yori',
     profession: 'CEO of Mila Academ, ex COO of the Moja Foundation',
     function: 'Quuen has been leading open sources project at the Moja Fondation such as the open source movement',
   },
 
   column6: {
-    image: 'images/speakers/img-1.jpg',
+    image: 'https://i.postimg.cc/hv2TvFtG/christopher-campbell-r-DEOVt-E7v-Os-unsplash.jpg',
     names: 'Sheryl Amanda',
     profession: 'Executive Director of Searcher Foundation',
     function: 'Sheryl is the executive Director of Searcher Foundation, the non profit organization that operates WikiMoja. Wiki moja is a freely available in 290 langages and used by nearly half a billion around the goal',
